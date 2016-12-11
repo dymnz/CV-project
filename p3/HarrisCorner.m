7dcbacb733d407fd84de6dbb3213e98c98fa2900
vicodin1123 <vicodin1123@gmail.com>
2016-12-11 16:24:32 +0800

    Img test

--- p3/HarrisCorner.m
+++ p3/HarrisCorner.m
@@ -1,6 +1,8 @@
+% Harris Corner with Non-maximum Suppression
 clear; close all;
 
-img = imread('./pattern.png');
+% img = imread('./pattern.png');
+img = imread('./s_bottom2_right.jpg');
 grayimg = rgb2gray(img);
 [H W] = size(grayimg);
 
@@ -15,8 +17,7 @@ dy2 = conv2(dy.^2, window, 'same');
 dxy = conv2(dx.*dy, window, 'same');
 
 k = 0.05;
-% cim = (dx2.*dy2 - dxy.^2) - k*(dx2 + dy2).^2; 
-cim = (dx2.*dy2 - dxy.^2)./(dx2 + dy2 + eps); % Harris corner measure
+cim = (dx2.*dy2 - dxy.^2)./(dx2 + dy2 + eps);
 
 NUMEL = 100;
 
@@ -25,21 +26,32 @@ for i = 1 : NUMEL
     [val ind] = max(cim(:));
     [row, col] = ind2sub(size(cim), ind);
     properCorners(i, :) = [row col];
-    for r = max(0, row - WINDOW) : min(row + WINDOW, H)
-        for c = max(0, col - WINDOW) : min(col + WINDOW, W)
+    for r = max(1, row - WINDOW) : min(row + WINDOW, H)
+        for c = max(1, col - WINDOW) : min(col + WINDOW, W)
             cim(r, c) = 0;
         end
     end    
 end
 cim = zeros(size(cim));
+% for i = 1 : NUMEL
+%     row = properCorners(i, 1); 
+%     col = properCorners(i, 2);
+%     for r = max(1, row - WINDOW) : min(row + WINDOW, H)
+%         for c = max(1, col - WINDOW) : min(col + WINDOW, W)
+%             cim(r, c) = 1;
+%         end
+%     end  
+% end
+% imshow(cim);
 for i = 1 : NUMEL
     row = properCorners(i, 1); 
     col = properCorners(i, 2);
-    for r = max(0, row - WINDOW) : min(row + WINDOW, H)
-        for c = max(0, col - WINDOW) : min(col + WINDOW, W)
-            cim(r, c) = 1;
+    for r = max(1, row - WINDOW) : min(row + WINDOW, H)
+        for c = max(1, col - WINDOW) : min(col + WINDOW, W)
+            img(r, c, :) = [255 0 0];
         end
     end  
 end
-imshow(cim);
+imshow(img);
+
 
